9f83569a94312f9ae68105109618cc02438348f1
Emmanuel Chargoy <charger513@hotmail.com>
2021-07-27 14:22:43 -0500

    Dependency Injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:clean_architecture_tdd_course/injection_container.dart' as di;
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await di.init();
   runApp(MyApp());
 }
 
